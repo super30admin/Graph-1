fb11b32bed81d08f1ed264bd2816442f271e9bc9
keerthitejakonuri <keerthi49100@gmail.com>
2019-12-07 12:05:55 -0600

    Maze implementation
    Maze implementation

--- Maze.java
+++ Maze.java
@@ -4,37 +4,46 @@
 // Any problem you faced while coding this : Nope.
 
 
-// Your code here along with comments explaining your approach
-class Maze {
+// Your code here along with comments explaining your approach: 
+Approached this problem using dfs and at each step we are traversing untill we reach boundary.
+
+class Solution {
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        boolean[][] visited = new boolean[maze.length][maze[0].length];
-        return dfs(maze, start, destination, visited);
+        if(null == maze || 0 == maze.length){return true;}
+        boolean visited[][] = new boolean[maze.length][maze[0].length];
+        return helper(maze,start,destination,visited);
     }
-    
-     public boolean dfs(int[][] maze, int[] start, int[] destination, boolean[][] visited) {
-        if (visited[start[0]][start[1]])
-            return false;
-        if (start[0] == destination[0] && start[1] == destination[1])
-            return true;
+    private boolean helper(int[][] maze, int[] start, int[] dest, boolean[][] visited){
+        if(visited[start[0]][start[1]]){return false;}
+        if(start[0] == dest[0] && start[1] == dest[1]){return true;}
         visited[start[0]][start[1]] = true;
-        int r = start[1] + 1, l = start[1] - 1, u = start[0] - 1, d = start[0] + 1;
-        while (r < maze[0].length && maze[start[0]][r] == 0) // right
-            r++;
-        if (dfs(maze, new int[] {start[0], r - 1}, destination, visited))
-            return true;
-        while (l >= 0 && maze[start[0]][l] == 0) //left
+        int l = start[1]-1, r = start[1] + 1, u = start[0] - 1, d = start[0] + 1;
+        int temp = start[1];
+        while(l>= 0 && maze[start[0]][l] == 0){
+            start[1] = l;
             l--;
-        if (dfs(maze, new int[] {start[0], l + 1}, destination, visited))
-            return true;
-        while (u >= 0 && maze[u][start[1]] == 0) //up
+        }
+        if(helper(maze,start,dest,visited)){return true;}
+        start[1] = temp;
+        while(r<maze[0].length && maze[start[0]][r] == 0){
+            start[1] = r;
+            r++;
+        }
+        if(helper(maze,start,dest,visited)){return true;}
+        start[1] = temp;
+        temp = start[0];
+        while(u >= 0 && maze[u][start[1]] == 0){
+            start[0] = u;
             u--;
-        if (dfs(maze, new int[] {u + 1, start[1]}, destination, visited))
-            return true;
-        while (d < maze.length && maze[d][start[1]] == 0) //down
+        }
+        if(helper(maze,start,dest,visited)){return true;}
+        start[0] = temp;
+        while(d < maze.length && maze[d][start[1]] == 0){
+            start[0] = d;
             d++;
-        if (dfs(maze, new int[] {d - 1, start[1]}, destination, visited))
-            return true;
-        return false;
+        }
+        if(helper(maze,start,dest,visited)){return true;}
+        start[0] = temp;
+    return false;
     }
-    
 }
