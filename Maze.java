9e508bd6ca5deb697c6fb8b8d73a1ca8720e4eab
Sachin Sharma <sachinsharma3191@gmail.com>
2020-02-28 20:53:54 -0800

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -1,71 +1,73 @@
+//Time Complexity : O(MN) 
+//Space Complexity : O(MN)
+//Did this code successfully run on Leetcode : Yes
+//Any problem you faced while coding this : No
 
 
-import java.util.*;
-
-public class Maze{
-	int dirs [][] = { {0,1},{0,-1},{-1,0},{1,0}};
+//Y
+public class Maze {
+	int dirs[][] = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
 	boolean visited[][];
-	int row ,col;
+	int row, col;
 
 	public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-     	if(maze == null || maze.length == 0 )
-     		return false;
-     	if(Arrays.equals(start,destination))
-     		return true;
-     	row = maze.length;
-     	col = maze[0].length;
-     	return bfs(maze,start,destination);
-    }
+		if (maze == null || maze.length == 0)
+			return false;
+		if (Arrays.equals(start, destination))
+			return true;
+		row = maze.length;
+		col = maze[0].length;
+		visited = new boolean[row][col];
+		return bfs(maze, start, destination);
+	}
 
-    public boolean bfs(int [][]maze,int []start,int []destination){
-    	Queue<int []> queue = new LinkedList<>();
+	public boolean bfs(int[][] maze, int[] start, int[] destination) {
+		Queue<int[]> queue = new LinkedList<>();
 
-    	queue.add(start);
-    	visited = new boolean[row][col];
-    	
-    	visited[start[0]][start[1]] = true;
+		queue.add(start);
+		visited[start[0]][start[1]] = true;
 
-    	while(!queue.isEmpty()){
-    		int []cell = queue.remove();
-    		if(cell[0] == destination[0] && cell[1]== destination[1])
-    			return true;
-    		
-    		for(int dir[] : dirs){
-    			int newRow = cell[0] + dir[0];
-    			int newCol = cell[1] + dir[1];
+		while (!queue.isEmpty()) {
+			int[] cell = queue.remove();
 
-    			while(isSafe(newRow,newCol) && maze[newRow][newCol] == 0 ){
-    				newRow += dir[0];
-    				newCol += dir[1];
-    			}
-    			System.out.println("NewRow:" + newRow + "-> NewCol:" + newCol);
-    			newRow -= dir[0];
-    			newCol -= dir[1];
+			if (Arrays.equals(cell, destination))
+				return true;
 
-    			if(!visited[newRow][newCol]){
-    				int []newEntry = new int[]{newRow,newCol};
-    				queue.add(newEntry);
-    				visited[newRow][newCol] = true;
-    			}
-    		}
-    	}
-    	return false;
-    }
+			for (int dir[] : dirs) {
+				int newRow = cell[0] + dir[0];
+				int newCol = cell[1] + dir[1];
 
-    public boolean isSafe(int r,int c){
-    	return r >= 0   && r < row  &&  c < col;
-    }
+				while (isSafe(newRow, newCol) && maze[newRow][newCol] != 1) {
+					newRow += dir[0];
+					newCol += dir[1];
+				}
+				newRow -= dir[0];
+				newCol -= dir[1];
 
-    public static void main(String args[]){
-    	int maze[][] = {{0,0,1,0,0},{0,0,0,0,0},{0,0,0,1,0},{1,1,0,1,1},{0,0,0,0,0}};
+				if (!visited[newRow][newCol]) {
+					queue.add(new int[] { newRow, newCol });
+					visited[newRow][newCol] = true;
+				}
+			}
+		}
+		return false;
+	}
 
-    	int start[] = {0,4};
-    	int destination[] = {4,4};
+	public boolean isSafe(int r, int c) {
+		return r >= 0 && c >= 0 && r < row && c < col;
+	}
 
-    	Maze m = new Maze();
+	public static void main(String args[]) {
+		int maze[][] = { { 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 0 }, { 1, 1, 0, 1, 1 },
+				{ 0, 0, 0, 0, 0 } };
 
-    	boolean isReachable = m.hasPath(maze,start,destination);
+		int start[] = { 0, 4 };
+		int destination[] = { 4, 4 };
 
-    	System.out.println(isReachable);
-    }
+		Maze m = new Maze();
+
+		boolean isReachable = m.hasPath(maze, start, destination);
+
+		System.out.println(isReachable);
+	}
 }
