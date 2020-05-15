85f095d1ab056ada30212d96fe8dfd0f2c858eb9
arcsoftech <arihant_Chhajed@yahoo.in>
2020-05-14 19:05:09 -0500

    Completed Graph-1

---
+++ Trust.java
@@ -0,0 +1,38 @@
+// Time Complexity : O(mn)
+// Space Complexity : O(mn)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+class Solution {
+    HashMap<Integer,HashSet<Integer>> map;
+    public int findJudge(int N, int[][] trust) {
+        map =  new HashMap<>();
+        
+       for(int[] rel:trust)
+       {
+           if(!map.containsKey(rel[0]))
+           {
+               map.put(rel[0],new HashSet<>());
+           }
+            map.get(rel[0]).add(rel[1]);
+          
+       }
+        int judge =1;
+        for(int i = 1;i <=N;i++)
+        {
+            if(i !=judge && trusts(judge,i))
+                judge = i;
+        }
+        for(int i = 1;i <=N;i++)
+        {
+            if(i == judge) continue;
+            if(trusts(judge,i) || !trusts(i,judge)) return -1;
+        }
+        return judge;
+    }
+    
+    private boolean trusts(int a, int b)
+    {
+        if(map.containsKey(a) && map.get(a).contains(b)) return true;
+        else return false;
+    }
+}
