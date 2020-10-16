a5b9b348c011827ef21ab25e0e6f21e3a70e8e28
yeolesne <42819574+yeolesne@users.noreply.github.com>
2020-10-15 22:36:46 -0700

    Town Judge

--- TownJudge.java
+++ TownJudge.java
@@ -1,15 +1,15 @@
 /*
  * #997. Find the Town Judge
  * 
- * In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town judge.
+ * In a town, there are N people labeled from 1 to N.  There is a rumor that one of these people is secretly the town judge.
 
 If the town judge exists, then:
 
-1. The town judge trusts nobody.
-2. Everybody (except for the town judge) trusts the town judge.
-3. There is exactly one person that satisfies properties 1 and 2.
+1. The town judge trusts nobody -> No outgoing edges
+2. Everybody (except for the town judge) trusts the town judge -> (N-1) incoming edges
+3. There is exactly one person that satisfies properties 1 and 2 -> Only one town judge
 
-You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person labelled b.
+You are given trust, an array of pairs trust[i] = [a, b] representing that the person labeled a trusts the person labeled b.
 
 If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1.
 
