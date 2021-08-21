6517b8db4feba36160ef0303c4cf838eca3a1a0c
DaryaniLeena <daryani.l@husky.neu.edu>
2021-08-20 23:52:54 -0700

    Added sol to Town Judge

--- TownJudge.java
+++ TownJudge.java
@@ -18,6 +18,30 @@ class Solution {
             }
         }
         return -1;
+    }
+}
+
+//******************************************* Using single array **************************************************
+//Time Complexity:O(Vertices + Edges)
+//Space Complexity: O(Vertices)
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        if(N==1){
+            return 1;
+        }
+        int[] indegree = new int[N];
+
+
+        for(int i=0;i<trust.length;i++){
+            indegree[trust[i][0]-1]--;
+            indegree[trust[i][1]-1]++;
+        }
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i] == N-1){
+                return i+1;
+            }
+        }
+        return -1;
 
 
     }
