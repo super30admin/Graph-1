767de580c703ed5bbc4e567bf93327bde88a494e
Durgesh Patil <patildurgesh1998@gmail.com>
2021-09-20 16:05:45 -0400

    Create Problem-1.cpp

---
+++ Problem-1.cpp
@@ -0,0 +1,23 @@
+/*
+TC = O(n)
+SC = O(N)
+where n is the size of the trust vector and N is the number of peoples.   
+*/
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(n==0)
+            return -1;
+        int i;
+        vector<int> indegree(n,0);
+        for(auto t : trust)
+        {
+            indegree[t[1]-1]++;
+            indegree[t[0]-1]--;
+        }
+        for(i=0;i<n;i++)
+            if(indegree[i]==n-1)
+                return i+1;
+        return -1;
+    }
+};
