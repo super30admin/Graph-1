0ba5d9efe373160a7d56e49a3eaa59a456ff33ab
Durgesh Patil <patildurgesh1998@gmail.com>
2021-09-21 19:33:23 -0400

    Update Problem-2.cpp

--- Problem-2.cpp
+++ Problem-2.cpp
@@ -2,6 +2,7 @@
 BFS Solution
 TC = O((m+n)(mn))
 SC = O(mn)
+where m is the number of rows and n is the number of coloumns.
 */
 class Solution {
 public:
@@ -38,6 +39,50 @@ public:
                 }
             }
         }
+        return false;
+    }
+};
+
+/*
+DFS Solution
+TC = O((m+n)(mn))
+SC = O(h)
+where m is the number of rows and n is the number of coloumns.
+where h is the height of the tree.
+That is how far we can traverse in one direction until one of the base case is hit.
+*/
+class Solution {
+public:
+    vector<vector<int>> dir;
+    int m,n;
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        m = maze.size();
+        n = maze[0].size();
+        dir = {{0,1},{0,-1},{1,0},{-1,0}};
+        return dfs(maze,start[0],start[1],destination);
+    }
+    bool dfs(vector<vector<int>> &maze, int row, int col, vector<int> &destination)
+    {
+        //base
+        if(row==destination[0] && col==destination[1])
+            return true;
+        
+        //logic
+        maze[row][col]=2;
+        for(auto d :dir)
+            {
+                int r = row;
+                int c = col;
+                while(r>=0 && r<m && c>=0 && c<n && maze[r][c]!=1)
+                {
+                    r = r + d[0];
+                    c = c + d[1];
+                }
+                r = r - d[0];
+                c = c - d[1];
+                if(maze[r][c]!=2 && dfs(maze,r,c,destination))
+                    return true;
+            }
         return false;
     }
 };
