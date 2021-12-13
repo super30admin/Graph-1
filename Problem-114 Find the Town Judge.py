3b002a2a15ce5eacca6b062974a0d55be231416c
Adhish Shrivastava <adhish70@gmail.com>
2021-12-13 09:45:06 -0500

    Initial Commit

---
+++ Problem-114 Find the Town Judge.py
@@ -0,0 +1,11 @@
+# 997. Find the Town Judge
+# https://leetcode.com/problems/find-the-town-judge/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        pass
---
+++ Problem-115 The Maze.py
@@ -0,0 +1,11 @@
+# 490. The Maze
+# https://leetcode.com/problems/the-maze/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def hasPath(self, maze, start, dest):
+        pass
