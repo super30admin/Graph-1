8206c9bff206c25a37e95c53f661ec57c9ff3451
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-06-11 14:12:03 -0700

    Add files via upload

---
+++ FindJudge.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    class FindJudge
+    {
+        /*
+         * T.C: O(V+E) where v is vertices (people) & e is edges
+         * S.C: O(V)
+         */
+        public int FindJudge(int n, int[][] trust)
+        {
+
+            if (n == 0) return -1;
+
+
+            int[] indegree = new int[n + 1];
+
+
+            foreach (int[] t in trust)
+            {
+                indegree[t[0]]--;
+                indegree[t[1]]++;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (indegree[i] == n - 1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
