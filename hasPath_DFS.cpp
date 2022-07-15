588e29e671d2c16ade3dd7dd9ec4c37bbe7f374e
Arjun Chaturvedi <chaturvedi.arjun.07@gmail.com>
2022-07-14 21:41:17 -0700

    Updated DFS solution for has path.

--- hasPath_DFS.cpp
+++ hasPath_DFS.cpp
@@ -9,55 +9,60 @@
 
 class Solution {
 public:
+    vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int m, n;  
     bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
-
+        
         if (maze.size() == 0)
         {
             return false;
         }
-
-        int m = maze.size();
-        int n = maze[0].size();
-
-        vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        queue<vector<int>> q;
-
-        maze[start[0]][start[1]] = 2;
-        q.push({start[0], start[1]});
-
-        while (!q.empty())
+        
+        m = maze.size();
+        n = maze[0].size();
+                
+        return dfs(maze, start[0], start[1], destination);
+    }
+    
+    bool dfs(vector<vector<int>>& maze, int& currRow, int& currCol, vector<int>& destination)
+    {
+        
+        // base case
+        
+        if (currRow == destination[0] && currCol == destination[1])
         {
-            int currRow = q.front()[0];
-            int currCol = q.front()[1];
-            q.pop();
-
-            for (const auto& dir : dirs)
-            {
-                int i = currRow;
-                int j = currCol;
-
-                while (i < m && i >= 0 && j < n && j >= 0 && maze[i][j] != 1)
-                {
-                    i += dir[0];
-                    j += dir[1];
-                }
-
-                i -= dir[0];
-                j -= dir[1];
-
-                if (i == destination[0] && j == destination[1])
-                {
-                    return true;
-                }
-
-                if (maze[i][j] == 0)
-                {
-                    maze[i][j] = 2;
-                    q.push({i, j});
-                }
-            }
+            return true;
+        }
+        
+        if (maze[currRow][currCol] == 2)
+        {
+            return false;
         }
 
+        // Logic
+        
+        maze[currRow][currCol] = 2;
+        
+        for (const auto& dir : dirs)
+        {
+            int i = currRow;
+            int j = currCol;
+                
+            while (i < m && i >= 0 && j < n && j >= 0 && maze[i][j] != 1)
+            {
+                i += dir[0];
+                j += dir[1];
+            }
+            
+            i -= dir[0];
+            j -= dir[1];
+                                
+            if (maze[i][j] == 0 && dfs(maze, i, j, destination))
+            {
+                return true;
+            }
+        }
+        
         return false;
     }
 };
