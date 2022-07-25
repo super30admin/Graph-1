93e7e3d2412c619bde4553064ada70d0895b5c86
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-07-25 11:36:38 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,1 +1,28 @@
+*****//114.997. FIND TOWN JUDGES****
+//Time complexity: 0(v+e);
+//Space complexity :0(n);
 
+  
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        
+        //Declaring array
+        int[] indegree=new int[n];
+        for(int[] t: trust)
+        {
+            indegree[t[0]-1]--;
+            indegree[t[1]-1]++;
+        }
+        
+        //Iterate through array
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==n-1)
+            {
+                return i+1;
+            }
+        }
+        
+        return -1;
+    }
+}
