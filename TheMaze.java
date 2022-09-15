f7d4d1ae002e0a09413ee38423a3d771e11062e7
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-15 08:26:04 -0700

    Create TheMaze.java

---
+++ TheMaze.java
@@ -0,0 +1,61 @@
+//Approach - 1: BFS; Use Queue add start 0,1; mark visited; pop and check all 4 directions
+//Time Complexity : O(k*mn) ==> O(mn)
+//Space Complexity : O(n) Queue
+
+class Solution {
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+
+        if(start[0] == destination[0] && start[1] == destination[1]) return true;
+        
+        int m = maze.length;
+        int n = maze[0].length;
+        
+        int [][] dirs = new int[][]{{-1,0}, {0,1}, {1,0}, {0,-1}}; //top right bottom left
+        
+        Queue<Integer> q = new LinkedList<>();
+            
+        //Queue<int []> q = new LinkedList<>(); // pairs of rows and columns.
+      
+        
+        //add as pairs 1 by 1
+        q.add(start[0]);
+        q.add(start[1]);
+        
+        maze[start[0]][start[1]] = 2; // Mark it as visited, no need go through that dir again.
+        
+        
+        while(!q.isEmpty()){
+            int r = q.poll();
+            int c = q.poll();
+            
+            for(int[] dir : dirs){
+                //current position
+                    int i = r;
+                    int j = c;
+ 
+                //if its 0 or 2 just we will pass through, till we reach walls or 1
+                while(i>= 0 && i<m && j>=0 && j<n && maze[i][j] != 1){
+                        //new direction valuws
+                    i = i + dir[0]; 
+                    j = j + dir[1];      
+                }
+                //to get previous indexes instead of 1 indexes // 1 step back
+                    i = i-dir[0];
+                    j = j-dir[1];
+                
+                //mark visited
+                if(maze[i][j] != 2){
+                    //check if its destination
+                    if(i == destination[0] && j == destination[1]) return true;
+                    
+                    //add to the queue
+                    q.add(i);
+                    q.add(j);
+                    maze[i][j] = 2;
+                }
+   
+            }
+        }
+        return false;
+    }
+}
