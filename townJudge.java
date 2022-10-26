d874b7c35c615192d7627ccfa67da46776ddaed2
Saurabh Kulkarni <srbhkulkarni13@gmail.com>
2022-10-26 04:20:37 +0000

    Graph-1 Submission.

---
+++ townJudge.java
@@ -0,0 +1,22 @@
+
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Understanding base cases
+
+public class Solution {
+	public int findJudge(int N, int[][] trust) {
+		 if (trust.length == 0)
+			  return N == 1 ? 1 : -1;
+		 int[] trustCount = new int[N + 1];
+		 for (int[] t : trust) {
+			  trustCount[t[1]]++;
+			  trustCount[t[0]]--;
+		 }
+		 for (int i = 1; i < trustCount.length; i++) {
+			  if (trustCount[i] == N - 1)
+					return i;
+		 }
+		 return -1;
+   }
+}
