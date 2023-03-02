a4bd00be937b35a05c532b45e4cbdfa9388eb587
jayavardhan3112 <jayavardhan3112@gmail.com>
2023-03-02 11:03:37 -0700

    Initial Commit

--- maze.py
+++ maze.py
@@ -3,14 +3,16 @@
 
 class Solution:
     def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
-        #Approach 1
+        # Approach 1
         def helper(i, j):
-            #Base
+            # Base
             # if i not in range(len(maze)) or j not in range(len(maze[0])): return False
-            if [i,j] == destination: return True
-            if maze[i][j] == 2: return False
+            if [i, j] == destination:
+                return True
+            if maze[i][j] == 2:
+                return False
             # print(i, j)
-            #Logic
+            # Logic
             maze[i][j] = 2
             for dire in dirs:
                 x = i
@@ -20,17 +22,19 @@ class Solution:
                     y += dire[1]
                 x -= dire[0]
                 y -= dire[1]
-                if helper(x, y): return True
+                if helper(x, y):
+                    return True
             return False
 
-        dirs = [[0,-1], [-1,0], [1,0], [0,1]]
+        dirs = [[0, -1], [-1, 0], [1, 0], [0, 1]]
         return helper(start[0], start[1])
-    
-        #Approach 2
-        if start == destination: return True
-        dirs = [[0,-1], [-1,0], [1,0], [0,1]]
+
+        # Approach 2
+        if start == destination:
+            return True
+        dirs = [[0, -1], [-1, 0], [1, 0], [0, 1]]
         visited = set()
-        
+
         q = deque()
         q.append(start)
         while q:
@@ -38,14 +42,15 @@ class Solution:
             for dire in dirs:
                 i = curr[0]
                 j = curr[1]
-                while i  >= 0 and i < len(maze) and j >= 0 and j < len(maze[0]) and maze[i][j] != 1:
+                while i >= 0 and i < len(maze) and j >= 0 and j < len(maze[0]) and maze[i][j] != 1:
                     i += dire[0]
                     j += dire[1]
-                
+
                 i -= dire[0]
                 j -= dire[1]
-                if [i,j] == destination: return True
+                if [i, j] == destination:
+                    return True
                 if maze[i][j] != 2:
                     maze[i][j] = 2
-                    q.append([i,j])
+                    q.append([i, j])
         return False
