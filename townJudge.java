ac31437163a17287d137200c666866ed6fcaa639
rutujamandlikFT <123407038+rutujamandlikFT@users.noreply.github.com>
2023-03-25 21:37:50 -0400

    Update townJudge.java

--- townJudge.java
+++ townJudge.java
@@ -1,1 +1,17 @@
 
+//TC: O(V+E)
+//SC: O(V)
+
+class Solution {
+    public int findJudge(int n, int[][] trusts) {
+        int[] indegrees = new int[n];
+        for(int[] trust : trusts ){
+            indegrees[trust[0]-1]--;
+            indegrees[trust[1]-1]++;
+        }
+        for(int i=0;i<n;i++){
+            if(indegrees[i] == n-1) return i+1;
+        }
+        return -1;
+    }
+}
