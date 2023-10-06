d22ca63e8a178b479322dc22f4da5de5c5822e3d
Gopi Vaka <vakagopi80@gmail.com>
2023-10-06 00:19:25 -0500

    completed Graph-1

---
+++ FindTheTownJudge.java
@@ -0,0 +1,19 @@
+//tc = O(v+e)
+//sc = O(v)
+
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] indegrees = new int[n];
+        for (int[] tr : trust) {
+            indegrees[tr[0] - 1]--;
+            indegrees[tr[1] - 1]++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (indegrees[i] == n - 1) {
+                return i + 1;
+            }
+        }
+        return -1;
+
+    }
+}
