07ff26c14fceb503e411d8276a612cc3dc1c2915
Sharathpatangi <sharathpatangi@gmail.com>
2023-11-28 17:46:29 -0800

    Completed Graph-2.

---
+++ TheMaze.java
@@ -0,0 +1,7 @@
+// Time Complexity : O(n)
+// Space Complexity : O(h)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : None
+
+public class TheMaze {
+}
